b94e3f8cca2455d7ea2706fddc7579ef2f9b453d
Madina89 <madinalinux89@gmail.com>
2019-08-25 16:01:35 -0500

    git added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -60,7 +60,7 @@ pipeline{
         }
         stage("Pull Repo"){
             steps{
-                git("https://github.com/farrukh90/packerde.git")
+                git("https://github.com/Madina89/packer_april.git")
             }
         }
         stage("Build Image"){
