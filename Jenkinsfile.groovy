2860a9daf02927de13be754a1553a0a6f1d8b433
Madina89 <madinalinux89@gmail.com>
2019-08-25 15:45:13 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -58,11 +58,16 @@ pipeline{
                 }
             }
         }
-        stage("Pull Repo"){
+               stage("Pull Repo"){
             steps{
-                git ("https://github.com/Madina89/packer_april.git")
-                sh "ls"
+                git("https://github.com/farrukh90/packerde.git")
+            }
+        }
+        stage("Build Image"){
+            steps{
+                sh "packer build updated/updated.json"
             }
         }
     }
 }
+            
