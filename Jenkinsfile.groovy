b4d99f766e8bfcfb6243953da4e0a9569943c568
Madina89 <madinalinux89@gmail.com>
2019-08-25 15:16:04 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,19 +12,19 @@ pipeline{
                 '''
             }
         }
-       tage("Download Terraform"){
+       stage("Download Terraform"){
             steps{
                 ws("tmp/"){
                     script {
                         def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
                         if (exists) {
                             sh "unzip -o terraform_0.12.7_linux_amd64.zip"
-                            sh "mv terraform /bin"
+                            sh "sudo mv terraform /bin"
                             sh "terraform version"
                         } else {
                             sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
                             sh "unzip -o terraform_0.12.7_linux_amd64.zip"
-                            sh "mv terraform /bin"
+                            sh "sudo mv terraform /bin"
                             sh "terraform version"
                         }
                     }
